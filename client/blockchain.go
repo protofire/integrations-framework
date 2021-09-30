1a1851bc010eac366c0513d270d2e094baf78ae5
koteld <festbot@gmail.com>
2021-10-01 00:08:09 +0300

    add evm_celo network type

--- client/blockchain.go
+++ client/blockchain.go
@@ -20,6 +20,7 @@ import (
 const (
 	BlockchainTypeEVM          = "evm"
 	BlockchainTypeEVMMultinode = "evm_multi"
+	BlockchainTypeEVMCelo      = "evm_celo"
 	NetworkGethPerformance     = "ethereum_geth_performance"
 )
 
@@ -53,6 +54,8 @@ func NewBlockchainClient(network BlockchainNetwork) (BlockchainClient, error) {
 		return NewEthereumClient(network)
 	case BlockchainTypeEVMMultinode:
 		return NewEthereumClients(network)
+	case BlockchainTypeEVMCelo:
+		return NewCeloClient(network)
 	}
 	return nil, errors.New("invalid blockchain network ID, not found")
 }
@@ -95,7 +98,7 @@ func NewNetworkFromConfig(conf *config.Config) (BlockchainNetwork, error) {
 		return nil, err
 	}
 	switch networkConfig.Type {
-	case BlockchainTypeEVM, BlockchainTypeEVMMultinode:
+	case BlockchainTypeEVM, BlockchainTypeEVMMultinode, BlockchainTypeEVMCelo:
 		return newEthereumNetwork(conf.Network, networkConfig)
 	}
 	return nil, fmt.Errorf(
